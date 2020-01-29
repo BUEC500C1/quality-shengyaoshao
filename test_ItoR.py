959f414051133cd0afcc553fe36f808d7567f7d3
shengyaoshao <55214118+shengyaoshao@users.noreply.github.com>
2020-01-28 22:37:20 -0500

    Update test_ItoR.py

--- test_ItoR.py
+++ test_ItoR.py
@@ -1,3 +1,4 @@
+"""
 print("enter the integer you want to convert to Roman Numeral")
 int1 = input()
 while (type(int1) != int):
@@ -6,14 +7,19 @@ while (type(int1) != int):
     except ValueError:
         print("input is not integer, please enter an integer")
         int1 = input()
-res = ''
-romancollection = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
-numbercollection = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
-i = 0
-while(int1 > 0):
-    if (int1 - numbercollection[i] >= 0):
-        res = res + romancollection[i]
-        int1 = int1 - numbercollection[i]
-    else:
-        i = i + 1
-print("the Roman Numeral representation of the integer you entered is",res)
+"""
+def convert(int):
+    res = ''
+    romancollection = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+    numbercollection = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    i = 0
+    while(int1 > 0):
+        if (int1 - numbercollection[i] >= 0):
+            res = res + romancollection[i]
+            int1 = int1 - numbercollection[i]
+        else:
+            i = i + 1
+    return res
+
+if __name == '__main__':
+    print(convert(342))
